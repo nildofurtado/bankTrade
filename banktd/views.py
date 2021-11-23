8e96d62fda9ab9f9af67c2cf6cb83a459143a2ff
Nildo Furtado <nildopd@gmail.com>
2021-11-23 03:23:56 -0300

    integracao com base

--- banktd/views.py
+++ banktd/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import CadUsuario
 
 class acesso():
     def index(request):
-        return HttpResponse('BankTrade')
+        r = CadUsuario.objects.filter(dtnasc__lte='1988-02-18').order_by('dtnasc')
+        return render(request, 'painel/index.html', { 'base' : r })
     
     def cadastro(request):
         return render(request, 'painel/cadastro.html', {})
